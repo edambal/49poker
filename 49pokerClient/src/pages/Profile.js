603b9e1d306f43a5d6d6996a7e3e870aa8b81e97
edambal <bal818edam@gmail.com>
2020-12-02 22:58:37 -0800

    Fixed the profile card placement

--- 49pokerClient/src/pages/Profile.js
+++ 49pokerClient/src/pages/Profile.js
@@ -68,19 +68,11 @@ class Profile extends React.Component {
   }
 
   render() {
-    // return (
-    //   <form onSubmit={this.handleSubmit}>
-    //     <label>
-    //       Name:
-    //       <input type="text" value={this.state.value} onChange={this.handleChange} />
-    //     </label>
-    //     <input type="submit" value="Submit" />
-    //   </form>
-    // );
+
     return (
       <>
-      {/* <Navbar /> */}
-      <MDBContainer>
+      <Navbar />
+      <MDBContainer className="profile-form"> 
         <MDBRow>
           <MDBCol md="6">
             <form onSubmit={this.handleSubmit}>
