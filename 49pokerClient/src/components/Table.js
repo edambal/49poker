b5837b8d67822bca260038faab8a6184d85527bc
edambal <bal818edam@gmail.com>
2020-12-02 21:46:05 -0800

    Buttons and Input Group Added

--- 49pokerClient/src/components/Table.js
+++ 49pokerClient/src/components/Table.js
@@ -1,22 +1,42 @@
 import React from 'react';
+import {Button,ButtonGroup, CardImg} from 'react-bootstrap';
+import {InputGroup} from 'react-bootstrap';
+import {FormControl} from 'react-bootstrap';
 import Pokercard from './Pokercard';
 import Playercard from './Playercard';
-import pokertable from '../images/custompoker.jpg'
+import pokertable from '../images/custompoker.jpg';
 import './Table.css'
 import Navbar from './Navbar';
 import Footer from './Footer';
 
 const Table = () =>{
     return(
-        <div>
-            <Navbar />
-            <img src={pokertable} title='Poker Table' />
-            <div className="card-player-dis">
-                <Pokercard/>
-                <Playercard/>
-            </div>
-            <Footer />
-        </div>
+      <div>
+      <Navbar />
+      <div class="col-md-20 col-md-push-40 offset-md-1 poker-table">
+      <CardImg src={pokertable} title='Poker Table' />
+      </div>
+     
+      <ButtonGroup aria-label="Basic example" className="control-buttons">
+      <Button variant="success">CALL</Button>
+      <Button variant="warning">FOLD</Button>
+      <Button variant="primary">CHECK</Button>
+      </ButtonGroup>
+      <InputGroup className="mb-3 raise-amount">
+      <InputGroup.Prepend>
+      <InputGroup.Text>$</InputGroup.Text>
+      </InputGroup.Prepend>
+      <FormControl aria-label="Amount (to the nearest dollar)" />
+      <InputGroup.Append>
+      <InputGroup.Text>.00</InputGroup.Text>
+      </InputGroup.Append>
+      </InputGroup>
+      <div className="card-player-dis">
+          <Pokercard/>
+          <Playercard/>
+      </div>
+      
+  </div>
     )
 }
 export default Table;
